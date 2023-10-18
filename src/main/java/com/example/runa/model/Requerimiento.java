6c237b4073d9c0e2c5bc55922a46834fe9c65c3f
Colichon Ramirez <jcolichon@intercorp.com.pe>
2023-10-17 19:48:08 -0500

    desplegando

--- src/main/java/com/example/runa/model/Requerimiento.java
+++ src/main/java/com/example/runa/model/Requerimiento.java
@@ -66,4 +66,12 @@ public class Requerimiento {
     public void setAsignacion(String asignacion) {
         this.asignacion = asignacion;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
