bda9287cd68297f34881c3e6d5bacc0ce14de6a8
Colichon Ramirez <jcolichon@intercorp.com.pe>
2023-10-17 22:19:29 -0500

    subiendo

--- src/main/java/com/example/runa/model/Usuario.java
+++ src/main/java/com/example/runa/model/Usuario.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.springframework.data.annotation.TypeAlias;
 
 @Entity
 public class Usuario {
