2b6312f8e332379fe186f6b3c27b4cb97a807aac
Valdemar <haroldferrm@gmail.com>
2024-06-29 12:07:18 -0500

    S

--- src/main/java/com/example/runa/model/Requerimiento.java
+++ src/main/java/com/example/runa/model/Requerimiento.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 
 //@Entity
 @Entity
-public class Requerimiento {
+public class Requerimiento { 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
