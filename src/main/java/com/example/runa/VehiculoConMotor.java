18958f27663cbe9bceb2a435fa34f78163661178
Juan Col <juanvcr@outlook.com>
2024-07-30 10:12:52 -0500

    asd

--- src/main/java/com/example/runa/VehiculoConMotor.java
+++ src/main/java/com/example/runa/VehiculoConMotor.java
@@ -2,5 +2,5 @@ package com.example.runa;
 
 public abstract class VehiculoConMotor extends Vehiculo{
 
-    public abstract void cargarCombustible();
+    public abstract void cargarCombustible(); //asdasd
 }
